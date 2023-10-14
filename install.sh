585ba35f428670568aed60e50d0ab7aa6e12103d
Prikolus <afterlifecircle@gmail.com>
2023-10-15 02:36:26 +0600

    Update install.sh
    частичная локаль, исправления установки лишних файлов

--- install.sh
+++ install.sh
@@ -6,38 +6,32 @@ cp_file()
     cp "${1}${3}" "${2}${3}"
 }
 
-
 # Check for root privileges
 if [ "$(id -u)" != "0" ]
 then
+	echo "Данный скрипт должен запускаться только с root привелегиями"
     echo "This script must be run with root privileges"
     exit 1
 fi
 
 # Install files etc.
+echo "Установка файлов..."
 echo "Installing files..."
 fromPath="./home/deck/.local/bin/"
 toPath="/home/deck/.local/bin/"
 mkdir -p $toPath
 cp_file $fromPath $toPath "allowadj.txt"
-cp_file $fromPath $toPath "curve.sh"
 cp_file $fromPath $toPath "experimental.sh"
 cp_file $fromPath $toPath "experimentaladj.txt"
 cp_file $fromPath $toPath "off.sh"
 cp_file $fromPath $toPath "on.sh"
 cp_file $fromPath $toPath "ryzenadj"
-cp_file $fromPath $toPath "set-ryzenadj-curve.sh"
-cp_file $fromPath $toPath "set-ryzenadj-tweaks.sh"
 cp_file $fromPath $toPath "statusadj.txt"
 
 fromPath="./etc/systemd/system/"
 toPath="/etc/systemd/system/"
 cp_file $fromPath $toPath "ac.target"
 cp_file $fromPath $toPath "battery.target"
-cp_file $fromPath $toPath "set-ryzenadj-curve.path"
-cp_file $fromPath $toPath "set-ryzenadj-curve.service"
-cp_file $fromPath $toPath "set-ryzenadj-tweaks.path"
-cp_file $fromPath $toPath "set-ryzenadj-tweaks.service"
 
 fromPath="./etc/udev/rules.d/"
 toPath="/etc/udev/rules.d/"
@@ -51,16 +45,36 @@ chmod 755 $toPath"experimental.sh"
 chmod 755 $toPath"on.sh"
 chmod 755 $toPath"off.sh"
 
+echo "Проверяем что андервольт отключен..."
 echo "Ensuring undervolt is off..."
 bash $toPath"off.sh"
 
+echo "Применяем новые правила электропитания..."
 echo "Enable new powertarget rules..."
 udevadm control --reload-rules
 
 while true; do
-    read -r -p "Select undervolt method: (all/curve)" ANSWER
+
+	echo "Для андервольта отдельно каждого ядра, введи: coper"
+	echo "For undervolt each core separately, enter: coper"
+	echo "Для андервольта всех ядер сразу, введи: coall"
+	echo "For undervolt all cores at once, enter: coall"
+	echo "Какой метод вы собираетесь использовать?"
+    read -r -p "What method you want use? " ANSWER
+	
     case $ANSWER in
-        a|all)
+	
+        coall|all)
+			
+			fromPath="./home/deck/.local/bin/"
+			toPath="/home/deck/.local/bin/"
+			cp_file $fromPath $toPath "set-ryzenadj-tweaks.sh"
+			
+			fromPath="./etc/systemd/system/"
+			toPath="/etc/systemd/system/"
+			cp_file $fromPath $toPath "set-ryzenadj-tweaks.path"
+			cp_file $fromPath $toPath "set-ryzenadj-tweaks.service"
+			
             echo "Enable path listener..."
             systemctl enable --now set-ryzenadj-tweaks.path
 
@@ -68,7 +82,19 @@ while true; do
             systemctl enable set-ryzenadj-tweaks.service
             break
             ;;
-        c|curve)
+			
+        coper|per)
+			
+			fromPath="./home/deck/.local/bin/"
+			toPath="/home/deck/.local/bin/"
+			cp_file $fromPath $toPath "curve.sh"
+			cp_file $fromPath $toPath "set-ryzenadj-curve.sh"
+			
+			fromPath="./etc/systemd/system/"
+			toPath="/etc/systemd/system/"
+			cp_file $fromPath $toPath "set-ryzenadj-curve.path"
+			cp_file $fromPath $toPath "set-ryzenadj-curve.service"
+			
             echo "Enable path listener..."
             systemctl enable --now set-ryzenadj-curve.path
 
@@ -94,6 +120,7 @@ echo "If the experimental setting also works fine you can edit"
 echo "the undervolt settings in the 'experimental' and"
 echo "'undervolt-on' sections of"
 echo "/home/deck/.local/bin/set-ryzenadj-tweaks.sh"
+echo "or /home/deck/.local/bin/set-ryzenadj-curve.sh"
 echo "script moving the -15 curve offset to the 'undervolt-on'"
 echo "section and making a more ambitous setting for the"
 echo "'experimental' section, e.g., a -20 curve offset."
